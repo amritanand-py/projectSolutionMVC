96eefcabb51bbf84a9421c563e3472258787e7ee
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-03-01 15:45:40 +0530

    file init

--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2955522d654b92d3f47fc690063bd0424df972c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
